f46905c31acf1316efcab46064babb63d37a129d
thomccc <thomas.arsenal.chen@gmail.com>
2026-01-28 10:22:00 -0800

    Fix /style-two and /style-three routes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,40 @@
 import { defineConfig } from 'vite'
+import path from 'node:path'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        home: path.resolve(__dirname, 'index.html'),
+        styleTwo: path.resolve(__dirname, 'style-two.html'),
+        styleThree: path.resolve(__dirname, 'style-three.html'),
+      },
+    },
+  },
   server: {
     port: 3000,
-    open: true
-  }
+    open: true,
+  },
+  plugins: [
+    {
+      name: 'pretty-multi-page-routes',
+      configureServer(server) {
+        server.middlewares.use((req, _res, next) => {
+          const url = req.url?.split('?')[0];
+          if (url === '/style-two') req.url = '/style-two.html';
+          if (url === '/style-three') req.url = '/style-three.html';
+          next();
+        });
+      },
+      configurePreviewServer(server) {
+        server.middlewares.use((req, _res, next) => {
+          const url = req.url?.split('?')[0];
+          if (url === '/style-two') req.url = '/style-two.html';
+          if (url === '/style-three') req.url = '/style-three.html';
+          next();
+        });
+      },
+    },
+  ],
 })
 
